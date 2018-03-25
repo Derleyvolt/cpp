bcd4b05bae426abab58bade8a2f1f0a475b396dd
StrawberrySwing2008 <35679266+StrawberrySwing2008@users.noreply.github.com>
2018-03-25 13:45:39 -0300

    Update and rename diferença entre árvores de busca binária to red-black tree vs AVL tree.cpp

---
+++ red-black tree vs AVL tree.cpp
@@ -0,0 +1,30 @@
+Àrvore AVL e Árvore rubro-negra
+
+QUAL A PRINCIPAL RAZÃO DE ESCOHER ÁRVORES RUBRO-NEGRAS AO INVÉS DE ÁRVORES AVL?
+
+Àrvores AVL: são rididamente balanceadas e consequetemente provém pesquisas mais rápidas. Portanto, se for fazer pesquisas intensivas
+             use uma árvore avl.
+             
+Para tarefas de uso intensivo de inserções utilize árvore rubro-negra.
+
+QUAL SÃO AS APLICAÇÕES DE RED-BLACK TREE?
+
+árvores rubro-negras são de propósitos gerais. Elas possuem desemepenho relativamente bom em adicionar, remover e pesquisar, mas árvores
+AVL tem pesquisas mais rapidas ao custo de adicionar e remover mais lentamente. Árvores rubro-negras são usadas em:
+
+C++ STL: map, multmap, multset.
+
+
+
+CONSIDERAÇÕES FINAIS:
+
+Árvores avl são mais balanceadas se comparadas com árvores rubro-negras, mas elas podem causar mais rotações durante a inserção e 
+deletação. Então se sua aplicação envolve frequentes inserções e deletações, então árvores rubro-negras devem ser preferidas. E
+se as inserções e deletações são menos frequentes e a operação de busca é mais frequente, então árvores avl devem ser preferidas.
+
+ 
+
+
+
+            
+            
