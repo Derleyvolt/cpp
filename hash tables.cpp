60f0a5d9a59c622b45973cd6e9514d845732116b
StrawberrySwing2008 <35679266+StrawberrySwing2008@users.noreply.github.com>
2018-03-26 16:15:58 -0300

    Update hash tables.cpp

--- hash tables.cpp
+++ hash tables.cpp
@@ -1,1 +1,13 @@
 
+Hash table:
+
+- Hash function
+- Key
+- Colisão
+- Encadeamento
+
+
+Descrição - 
+  
+  Hash table ou tabela de espalahamento é uma estrutura de dados especial, que associa chaves com pesquisa de valores. Seu objeto é,
+  apartir de chaves simples, fazer uma busca rápida e obter o valor desejado.
