4ef8269ee6eb1399ab0055f7e0537bb7c0496f6e
StrawberrySwing2008 <35679266+StrawberrySwing2008@users.noreply.github.com>
2018-04-02 19:56:49 -0300

    Update vector.cpp

--- STL/container/sequence containers/vector.cpp
+++ STL/container/sequence containers/vector.cpp
@@ -8,26 +8,37 @@
 2 - Lento na inserção e remoção no meio do container causam lentidão pois todos os outros elementos acima da posição inserida/deletada
     tem suas posições movidas.
     
-3 - rápido na inserção e remoção de elementos que estão no final do container pois não há elementos para serem movidos.
+3 - Rápido na inserção e remoção de elementos que estão no final do container pois não há elementos para serem movidos.
 
 
 
-
-
-o metodo capacity() retorna a quantidade de elementos que o vetor suporta no momento, diferente do metodo size(), por exemplo,
+O metodo capacity() retorna a quantidade de elementos que o vetor suporta no momento, diferente do metodo size(), por exemplo,
 se resevar-mos 10 espaços pro vector através do metodo reserve(), o metodo size() retornaria 0, enquanto o capacity() retornaria 10.
 
 A 'capacity' do vetor é importante por duas razões:
 
-  1. Realocação invalida todas as referências, ponteiros, e iterators para elementos do vetor.
+  1. Realocação inválida todas as referências, ponteiros, e iterators para elementos do vetor.
   2. Ralocação leva tempo.
   
-  para evitar realocação, você pode usar reserve() para assegurar uma certa capacidade antes de você realmente precisar disto.
-  Desta maneira, você pode ter 
-  To avoid reallocation, you can use reserve() to ensure a certain capacity before you really need
-it. In this way, you can ensure that references remain valid as long as the capacity is not exceeded:
-std::vector<int> v; // create an empty vector
-v.reserve(80); // reserve memory for 80 elements
+  Para evitar realocação, você pode usar reserve() para assegurar uma certa capacidade antes de você realmente precisar disto.
+  Desta maneira, você pode ter certeza que referências permaneçam válidas enquanto a capacidade não for excedida.
+
+  Basicamente o vector faz uma cópia do elemento quando o elemento é inserido, quando a capacidade do vector é excedida o vector
+  vai recolocar todos os elementos em um array maior, então o construtor de cópia/move de todos os elementos são chamados, tornado
+  assim a realocação uma operação pesada, use ponteiros ou smart pointers.
+      
+  std::vector<int> v;  // cria um vetor vazio
+  v.reserve(80);       // reserva memória para 80 elementos
+
+  
+  std::vector<some> vec;
+
+  vec.push_back(something); // copy é chamado.
+  vec.push_back(something); // copy é chamado e o vetor é realocado, sendo assim, o primeiro indice tem seu copy chamado novamente
+                            // pro novo array alocado.
+  vec.push_back(something); // copy é chamado e o vetor é realocado, os 2 primeiros indices tem seus copy chamados novamente..
+
+  com total de 6 copys chamados..
 
  
 
