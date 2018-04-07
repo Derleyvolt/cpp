c450dcb68291c7ed21e2ed3e0aedf52c6ab8e71c
StrawberrySwing2008 <35679266+StrawberrySwing2008@users.noreply.github.com>
2018-04-07 15:01:29 -0300

    Update and rename relações de equivalência.cpp to relações de equivalência e ordem.cpp

---
+++ relações de equivalência e ordem.cpp
@@ -0,0 +1,37 @@
+Uma demo sobre relações de equivalência e ordens
+
+A relação R é
+
+Reflexiva se x R x é true para todos os x
+
+Irreflexiva se x R x é false para todos os x
+
+Simétrico se quando x R y for true, então y R x também deve ser
+  
+Anti-simétrico se quando x R y é true então y R x é false
+
+Transitivo se quando x R y e y R z for true, então x R z também deve ser
+
+A relação = é reflexiva, simétrica e transitivo. A relação < é irreflexiva e transitiva. A relação ≤ é reflexiva e transitiva.
+
+Uma relação binária é considerada de partial order se ela for:
+
+1 - reflexiva
+2 - antisimetrica
+3 - transitiva
+
+Uma relação binária é considerada uma relação de equivalência se ela for:
+
+1 - reflexiva
+2 - simétrica
+3 - transitiva
+
+Dados retirados desse site -> https://www.eecis.udel.edu/~saunders/courses/320/10s/order-relation.html
+
+Properties of relations: Let R(T, T) be a relation on T. 
+R is reflexive if for all a in T, R(a,a) is true. 
+R is anti-reflexive if for all a in T, R(a,a) is false. 
+R is symmetric if for all a,b in T, if whenever R(a,b) is true, then R(b,a) is also true. 
+R is anti-symmetric if for all a,b in T, if whenever R(a,b) is true, then R(b,a) is false. 
+R is transitive if for all a,b,c in T, if when R(a,b) and R(b,c) are true, then R(a,c) is also true. 
+
