ba3a903e7770bae1e57c09649b58acded53aa8fd
StrawberrySwing2008 <35679266+StrawberrySwing2008@users.noreply.github.com>
2018-04-07 12:19:35 -0300

    Create relações de equivalência.cpp

---
+++ relações de equivalência.cpp
@@ -0,0 +1,59 @@
+A relation R is
+
+Reflexive if xRx is true for all x
+
+Irreflexive if xRx is false for all x
+
+Symmetric if when xRy, then yRx
+
+Transitive if when xRy and yRz, then xRz.
+
+The relation = is reflexive, symmetric, and transitive. The relation < is irreflexive and transitive. The relation ≤ is reflexive
+and transitive.
+
+
+
+
+
+
+
+
+
+
+
+R é a relação.
+
+Uma relação é chamada de REFLEXIVA se:
+
+- se x R x é true.
+
+Uma relação é chama de IRREFLEXIVA se:
+
+- se x R x é false.
+
+Uma relação é chamada de SIMÉTRICA se para os números reais x e y:
+
+- se x R y é true, então y R x também deve ser.
+
+Uma relação é chamada de TRANSITIVA se para os números x, y e z:
+
+- se x R y é true e y R z também é true, então x R z também
+  deve ser.
+  
+Uma relação é chamada de ANTI-SIMÉTRICA se para os números x, e y:
+
+- se x R y é true e y R x também for true, então 
+
+
+Uma relação binária é considerada de partial order se ela for:
+
+1 - reflexiva
+2 - antisimetrica
+3 - transitiva
+
+uma relação binária é considerada uma relação de equivalência se ela for:
+
+1 - reflexiva
+2 - simétrica
+3 - transitiva
+
