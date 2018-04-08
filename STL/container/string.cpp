7164f1bf496c5c408a71cfe4e727650b5b5eee1d
StrawberrySwing2008 <35679266+StrawberrySwing2008@users.noreply.github.com>
2018-04-08 20:29:32 -0300

    Update string.cpp

--- STL/container/string.cpp
+++ STL/container/string.cpp
@@ -1,1 +1,26 @@
 
+  vai ordenar os caracteres do container string..
+
+  std::string str = "hello world";
+
+  std::sort(str.begin(), str.end());
+
+	for ( auto& i : v )
+	{
+      std::cout << i.c_str() << std::endl;
+	}
+   
+  vai ordenar as palavras por ordem ...
+
+  std::vector<std::string> str = { "rato", "gato", "vaca", "galinha", "coruja" }; 
+
+  std::sort(str.begin(), str.end());
+
+  for ( auto& i : v )
+	{
+      std::cout << i.c_str() << std::endl;
+	}  
+
+  
+
+  
