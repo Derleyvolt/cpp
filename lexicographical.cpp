50e0007ca5b8660c058f02dd374656e6dec14a65
StrawberrySwing2008 <35679266+StrawberrySwing2008@users.noreply.github.com>
2018-04-09 02:43:58 -0300

    Update lexicographical.cpp

--- lexicographical.cpp
+++ lexicographical.cpp
@@ -1,14 +1,23 @@
 Alphabetical order is sorting words or strings created from a certain alphabet using the following method quoted from Wikipedia:
 
-To determine which of two strings comes first in alphabetical order, their first letters are compared. If they differ, then the string whose first letter comes earlier in the alphabet is the one which comes first in alphabetical order. If the first letters are the same, then the second letters are compared, and so on. If a position is reached where one string has no more letters to compare while the other does, then the first (shorter) string is deemed to come first in alphabetical order.
+To determine which of two strings comes first in alphabetical order, their first letters are compared. If they differ, then the string
+whose first letter comes earlier in the alphabet is the one which comes first in alphabetical order. If the first letters are the same,
+then the second letters are compared, and so on. If a position is reached where one string has no more letters to compare while the other
+does, then the first (shorter) string is deemed to come first in alphabetical order.
 
-The order of letters are determined by the alphabet that was used. The English alphabet only contains a-z and A-Z, so it only makes sense to alphabetically sort English words created using characters from a-z and A-Z. If your strings have numbers too, then you would alphanumerically order them.
+The order of letters are determined by the alphabet that was used. The English alphabet only contains a-z and A-Z, so it only makes sense
+to alphabetically sort English words created using characters from a-z and A-Z. If your strings have numbers too, then you would 
+alphanumerically order them.
 
-Lexicographical order is a generalization of alphabetical order, and it has a formal mathematical definition in terms of the Cartesian product of two sets: Lexicographical order. When sorting strings made up of characters, which can include symbols and glyphs, lexicographical order and alphabetical order are the same thing. You just have to define an alphabet which orders the characters that you use to create the strings. Lexicographical order differs from alphabetical order in the sense that you can lexicographically order more things than just words. You can order things such as mathematical sets, but people rarely use lexicographical order in this context.
+Lexicographical order is a generalization of alphabetical order, and it has a formal mathematical definition in terms of the Cartesian
+product of two sets: Lexicographical order. When sorting strings made up of characters, which can include symbols and glyphs,
+lexicographical order and alphabetical order are the same thing. You just have to define an alphabet which orders the characters that you
+use to create the strings. Lexicographical order differs from alphabetical order in the sense that you can lexicographically order more
+things than just words. You can order things such as mathematical sets, but people rarely use lexicographical order in this context.
 
-Note: Length comparison does NOT precede alphabetical comparison for Lexicographical order as stated by Hong-Yi Dai. For example, bc lexicographically comes after abc.
+Note: Length comparison does NOT precede alphabetical comparison for Lexicographical order as stated by Hong-Yi Dai. For example, bc
+lexicographically comes after abc.
 
 
 
 https://www.quora.com/Whats-the-difference-between-lexicographical-and-alphabetical-order
-a
