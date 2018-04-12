8a23c843a6f849b4d0c2c574fd73dac226943abf
StrawberrySwing2008 <35679266+StrawberrySwing2008@users.noreply.github.com>
2018-04-11 21:38:30 -0300

    Update predicates.cpp

--- STL/predicates.cpp
+++ STL/predicates.cpp
@@ -1,1 +1,9 @@
 
+
+Predicates é uma função que retorna um valor boleano ou uma instancia de um objeto tendo bool opeartor() como membro (functor). Unary predicate leva um argumento, binary leva dois, etc. 
+
+Um predicate deve sempre retornar o mesmo resultado diante do mesmo valor.
+  
+Predicate is a C++ function returning boolean or instance of object having bool operator() member. Unary predicate take one agrument, binary - two, etc.
+  
+Predicates são frequetementes usados nas rotinas de busca da STL, exemplo, copy_if, find_if, remove_if etc.
